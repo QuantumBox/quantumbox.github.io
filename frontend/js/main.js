42c5f2926c5553944877553162a53b0b26f14968
Arthur Ward <ssbfalcon@gmail.com>
2014-11-09 17:55:55 -0500

    Some style adjustments.
    Click on posts to stand out a bit more

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,1 +1,18 @@
+$(document).ready(function () {
+    $('nav.top').click(function () {
+        $(this).toggleClass('active');
+    });
+    
+    $('article.post').click(function () {
+        if(!$(this).hasClass('active'))
+        {
+            $('article.post').removeClass('active');  
+            $(this).addClass('active');
+        }
+    });
+    
+    $('article.post').mouseleave(function () {
+        $('article.post').removeClass('active');   
+    });
 
+});
